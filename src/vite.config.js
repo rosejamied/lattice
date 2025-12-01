be60a9f3154d86298cbaf899a5db19ca4e335728
rosejamied <rose.jamied@gmail.com>
2025-12-01 10:52:43 +0000

    Change API proxy target to new IP address

--- src/vite.config.js
+++ src/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Proxy /api requests to your backend server.
       '/api': {
-        target: 'http://localhost:2707',
+        target: 'http://81.151.30.245:2707',
         changeOrigin: true,
       },
     }
